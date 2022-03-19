31e153ceaf6efdb7291941fb94fce13a38b91ce4
Eyal Bardugo <taliandeyal@gmail.com>
2022-03-19 20:54:21 +0200

    Pass parmeter to child component - by @Input decorator

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Input1Component } from './input1/input1.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Input1Component
   ],
   imports: [
     BrowserModule,
